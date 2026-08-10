f9e9402a11fad91348a087ff070693d2e99c5852
nidhish654 <nidhish.shettigar.06@gmail.com>
2026-08-10 18:51:53 +0530

    Income-Expense Chart UI fix

--- src/features/dashboard/components/charts/income-expense-chart.tsx
+++ src/features/dashboard/components/charts/income-expense-chart.tsx
@@ -94,7 +94,7 @@ export default function IncomeExpenseChart({
                                 Number(value),
                                 currency
                             ),
-                            name === "income"
+                            name === "Income"
                                 ? "Income"
                                 : "Expense",
                         ]}
